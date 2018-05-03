6247633ece6c0596a5d602555e91ff489841a978
Filip Malinowski <crimhead@gmail.com>
2018-05-03 15:08:09 +0200

    enable camelCase for css-loader

--- css-modules.js
+++ css-modules.js
@@ -4,7 +4,7 @@ module.exports = {
   plugins: ['css-modules'],
   rules: {
     // https://github.com/atfzl/eslint-plugin-css-modules
-    'css-modules/no-unused-class': 'error',
-    'css-modules/no-undef-class': 'error'
+    'css-modules/no-unused-class': ['error', { camelCase: true }],
+    'css-modules/no-undef-class': ['error', { camelCase: true }]
   }
 };
