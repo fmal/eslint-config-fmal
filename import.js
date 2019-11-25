3f5e508fb0e87f87f9786c9fc180413f8914a993
Filip Malinowski <crimhead@gmail.com>
2019-11-25 13:04:48 +0100

    additional `import` rules

--- import.js
+++ import.js
@@ -4,9 +4,14 @@ module.exports = {
   plugins: ['import'],
   rules: {
     // https://github.com/benmosher/eslint-plugin-import
+    // some of the rules are inherited from
+    // https://github.com/standard/eslint-config-standard/blob/master/eslintrc.json
     'import/named': 'error',
     'import/default': 'error',
+    'import/no-dynamic-require': 'error',
     'import/no-absolute-path': 'error',
+    'import/no-useless-path-segments': 'error',
+    'import/no-extraneous-dependencies': 'error',
     'import/newline-after-import': 'warn',
     'import/no-commonjs': 'error',
     'import/no-amd': 'error'
