3c15e282d1588a9ff26bfab1384c530671328e47
Filip Malinowski <crimhead@gmail.com>
2022-01-29 21:49:24 +0100

    add imports ordering

--- import.js
+++ import.js
@@ -9,12 +9,23 @@ module.exports = {
     'import/named': 'error',
     'import/default': 'error',
     'import/no-dynamic-require': 'error',
-    'import/no-absolute-path': 'error',
+    'import/no-unresolved': 'error',
     'import/no-useless-path-segments': 'error',
     'import/no-extraneous-dependencies': 'error',
-    'import/newline-after-import': 'warn',
+    'import/newline-after-import': 'error',
     'import/no-commonjs': 'error',
-    'import/no-amd': 'error'
+    'import/no-amd': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], 'internal'],
+        pathGroups: [{ pattern: '{@/**,~/**}', group: 'internal' }],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc'
+        }
+      }
+    ]
   },
   settings: {
     'import/ignore': [
@@ -22,6 +33,7 @@ module.exports = {
       '.json$',
       '.(css|scss)$',
       '.(jpg|png|gif|svg|html|txt|md|woff|woff2|ttf|eot)$'
-    ]
+    ],
+    'import/external-module-folders': ['node_modules', '.yarn']
   }
 };
